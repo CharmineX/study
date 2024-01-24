b92e1644adc357eec900d80352777d274db063be
CharmineX <boedhramkenneth@gmail.com>
2024-01-24 21:38:32 +0100

    update

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -1,4 +1,4 @@
-@set "VIRTUAL_ENV=C:\Users\boedh\OneDrive\Bureaublad\myenv"
+@set "VIRTUAL_ENV=C:\Users\CharmineX\Desktop\study\myenv"
 
 @set "VIRTUAL_ENV_PROMPT="
 @if NOT DEFINED VIRTUAL_ENV_PROMPT (
